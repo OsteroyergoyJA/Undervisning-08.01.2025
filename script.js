e7a43f629558dd5bef27f44205aba2bb693c0352
OsteroyergoyJA <oosteroyergoy@gmail.com>
2025-01-08 11:36:53 +0100

    Not function

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log('Hello!');
+document.getElementById('add').addEventListener('click', function () {
+    let inputValue = document.getElementById('to-do').value;
+if (inputValue) {addTodo(inputValue);
+document.getElementById('to-do').value="";}
+});
+
+function addTodo(task) {
+    let todoList = document.getElementById('list');
+    let li= document.createElement('li');
+    li.textContent = task;
+
+    let removeButton = document.createElement('button');
+    removeButton.textContent = 'Fjern';
+    removeButton.onclick = function () {
+        todoList.removeChild(li);
+    };
+
+    li.appendChild(removeButton);
+    li.addEventListener('click', function(){
+        li.classList.toggle('completed');
+    });
+
+    todoList.appendChild(li);
+
+};
