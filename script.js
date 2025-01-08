67424f55d3231227a78615875b99922dae08d97b
OsteroyergoyJA <oosteroyergoy@gmail.com>
2025-01-08 13:18:27 +0100

    Siste lagring

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 document.getElementById('add').addEventListener('click', function () {
-    let inputValue = document.getElementById('to-do').value;
-if (inputValue) {addTodo(inputValue);
+    let inputValue = document.getElementById('to-do').value.trim();
+if (inputValue) {
+    addTodo(inputValue);
+    savedTooLocalStorage(inputValue, false);
 document.getElementById('to-do').value="";}
 });
 
-function addTodo(task) {
+function addTodo(item, isCompleted = false) {
     let todoList = document.getElementById('liste');
     const li= document.createElement('li');
-    li.textContent = task;
+    li.textContent = item;
+
+    if (isCompleted) { 
+        li.classlist.add("completed");
+    }
 
     let removeButton = document.createElement('button');
     removeButton.textContent = 'Fjern';
@@ -23,3 +29,19 @@ function addTodo(task) {
     todoList.appendChild(li);
 
 };
+
+
+
+// Local storage:
+
+document.addEventListener("DOMContentLoaded", function() {
+    let savedItems = JSON.parse(localStorage.getItem("items") )|| [];
+    savedItems.forEach(item => addItem(task.text, task.completed));
+        
+    });
+
+    function saveTodoLocalStorage(item, isCompleted) {
+        const todos = JSON.parse(localStorage.getItem('todos')) || [];
+        todos.push({text: item, completed: isCompleted});
+        localStorage.setItem('todos', JSON.stringify(todos));
+    };
